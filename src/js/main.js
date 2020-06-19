3660efc5ef3cb331c110c6504f417e739fad074f
b1mind <brent@1mindsolutions.com>
2020-06-18 19:24:36 -0600

    📱Mobile Menu 🐧Playing Dressup

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 import './app.js'
+
+const nav = document.getElementsByTagName('nav')[0]
+const canHazBurger = document.querySelector('.canHazBurger')
+
+canHazBurger.addEventListener('click', e => {
+	nav.classList.toggle('hidden')
+})
